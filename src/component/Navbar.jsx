851dfc3964f75494dc66c0756d63464178b3e5e4
rafika khatun <rafikakhatun607@gmail.com>
2025-07-17 23:17:36 +0530

    Replace logo with transparent PNG for better background blending

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,17 @@
+import logo from "../assets/Group 33.jpg"
+
 function Navbar(){
     return(
         <>
        <header className="w-full p-8 bg-[#1a2116]">
-        <nav>
-
+        <nav className="flex justify-center items-center" >
+          {/* nav logo */}
+          <div>
+            <img style={{backgroundColor:"transparent"}} src={logo} alt="logo" />
+            <h1>Planto</h1>
+          </div>
+           {/* nav items */}
+            {/* nav icon */}
         </nav>
        </header>
         
