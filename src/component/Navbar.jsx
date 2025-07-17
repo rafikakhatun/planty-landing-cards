57e4c68e111e9d8319aa5e2baf7e3be0ec7363e8
rafika khatun <rafikakhatun607@gmail.com>
2025-07-17 23:56:00 +0530

    Build Navbar UI with logo and menu items

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,20 +1,34 @@
+import { ChevronDown, Lock, Menu, Search } from "lucide-react";
 import logo from "../assets/Group 33.jpg"
 
-function Navbar(){
-    return(
+function Navbar() {
+    return (
         <>
-       <header className="w-full p-8 bg-[#1a2116]">
-        <nav className="flex justify-center items-center" >
-          {/* nav logo */}
-          <div>
-            <img style={{backgroundColor:"transparent"}} src={logo} alt="logo" />
-            <h1>Planto</h1>
-          </div>
-           {/* nav items */}
-            {/* nav icon */}
-        </nav>
-       </header>
-        
+            <header className="w-full p-2 bg-[#1a2116]">
+                <nav className="flex justify-between items-center text-[#C6C7C5] p-2" >
+                    {/* nav logo */}
+                    <div className="flex mt-4">
+                        <img className="h-14 w-16 mt-1" style={{ backgroundColor: "transparent" }} src={logo} alt="logo" />
+                        <h1 className="text-xl mr-10 z-10">Planto</h1>
+                    </div>
+                    {/* nav items */}
+                    <ul className="flex gap-5">
+                        <li>Home</li>
+                        <li className="flex">Plants Type <ChevronDown /></li>
+                        <li>More</li>
+                        <li>Contact</li>
+                    </ul>
+                    {/* nav icon */}
+                    <div className="flex gap-4">
+                        <Search className="w-5 h-5" />
+                        <Lock className="w-5 h-5" />
+                        <Menu className="w-6 h-6" />
+                    </div>
+
+
+                </nav>
+            </header>
+
         </>
     )
 }
