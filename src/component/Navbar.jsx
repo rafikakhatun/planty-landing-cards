6c4e1d23c5acb111294c5414115cb5828899bc47
rafika khatun <rafikakhatun607@gmail.com>
2025-07-19 20:11:04 +0530

    feat: Add hamburger menu button for mobile view

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -7,24 +7,30 @@ function Navbar() {
             <header className="w-full p-2 bg-[#1a2116]">
                 <nav className="flex justify-between items-center text-[#C6C7C5] p-2" >
                     {/* nav logo */}
-                    <div className="flex mt-4">
-                        <img className="h-14 w-16 mt-1" style={{ backgroundColor: "transparent" }} src={logo} alt="logo" />
-                        <h1 className="text-xl mr-10 z-10">Planto</h1>
-                    </div>
-                    {/* nav items */}
-                    <ul className="flex gap-5">
-                        <li>Home</li>
-                        <li className="flex">Plants Type <ChevronDown /></li>
-                        <li>More</li>
-                        <li>Contact</li>
-                    </ul>
-                    {/* nav icon */}
-                    <div className="flex gap-4">
-                        <Search className="w-5 h-5" />
-                        <Lock className="w-5 h-5" />
-                        <Menu className="w-6 h-6" />
+                    <div className="relative w-20 h-20">
+                        <img src={logo} className="w-full h-full object-cover mt-6" alt="logo" />
+                        <h1 className="absolute top-6 left-10 text-white text-xl font-bold z-10">
+                            Planto
+                        </h1>
                     </div>
 
+                    {/* desktop Menu */}
+                    <ul className=" hidden md:flex gap-5 px-4">
+                        <li className=" hover:underline">Home</li>
+                        <li className="flex  hover:underline">Plants Type <ChevronDown /></li>
+                        <li className=" hover:underline">More</li>
+                        <li className=" hover:underline">Contact</li>
+                    </ul>
+
+
+                    {/* Hamburger Menu Button for Small Devices */}
+                    <button className="flex md:hidden">
+                        <Menu className="w-10 h-10" />
+                        </button>
+
+
+
+                   
 
                 </nav>
             </header>
